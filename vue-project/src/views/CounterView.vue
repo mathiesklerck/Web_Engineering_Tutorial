36d4a20508b8a0f6352ed62fd694bf30a9c1e56b
Mathies Klerck <mathiesklerck@MBP-von-Mathies.fritz.box>
2023-05-22 21:32:34 +0200

    24th chapter

---
+++ vue-project/src/views/CounterView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import {counter} from "@/stores/counterStore";
+
+</script>
+
+<template>
+  <div>
+    
+    <h1>{{ counter.count }}</h1>
+
+    <button @click="counter.increment()">Erhöhen</button>
+  </div>
+
+
+</template>
