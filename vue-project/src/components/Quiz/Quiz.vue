086937098c6351f2231632e8a8b7341f5763da38
Mathies Klerck <mathiesklerck@MacBook-Pro-von-Mathies.fritz.box>
2023-05-22 16:25:21 +0200

    22th chapter

---
+++ vue-project/src/components/Quiz/Quiz.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <QuizHeader/>
+
+    <QuizQuestion/>
+    <QuizFooter :quiz="quiz"/>
+
+
+</template>
+
+<script setup>
+    import QuizHeader from "@/components/Quiz/QuizHeader.vue";
+    
+    import QuizQuestion from "@/components/Quiz/QuizQuestion.vue";
+    import QuizFooter from "@/components/Quiz/QuizFooter.vue";
+
+    defineProps({quiz: Object})
+
+    let name = ref('Lasse Muetze');
+
+    provide('name', {
+        name, 
+        changeName: () => name.value = 'Changed'
+    });
+</script>
